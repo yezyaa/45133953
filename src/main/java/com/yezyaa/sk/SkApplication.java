4cb07260e2648340afa07e06513c2ba3b27bbe83
yezy <iyeahji@gmail.com>
2024-11-30 11:51:08 +0900

    init commit

---
+++ src/main/java/com/yezyaa/sk/SkApplication.java
@@ -0,0 +1,13 @@
+package com.yezyaa.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkApplication.class, args);
+    }
+
+}
