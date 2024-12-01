3ebd72bdb060ddcef2745c3dbb11a877d6238bbc
yezy <iyeahji@gmail.com>
2024-12-01 13:46:04 +0900

    enhance: 로그인 예외 처리 추가
    - 잘못된 이메일 및 비밀번호 예외 처리

--- src/main/java/com/yezyaa/sk/global/exception/errorcode/AuthErrorCode.java
+++ src/main/java/com/yezyaa/sk/global/exception/errorcode/AuthErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "중복된 이메일이 존재합니다."),
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_EMAIL_OR_PASSWORD(HttpStatus.BAD_REQUEST, "이메일 또는 비밀번호가 잘못되었습니다.");
 
     private final HttpStatus status;
     private final String message;
