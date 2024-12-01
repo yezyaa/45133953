ef730a852cb8b93bf9129163629dde231a2fa616
yezy <iyeahji@gmail.com>
2024-12-01 23:24:32 +0900

    feat: 게시글 작성 API 기능 구현
    - BoardCreateRequest
      - 첨부파일 최대 5개의 파일 업로드 제한
    - BoardController
      - 게시글 작성 API 구현
      - @ModelAttribute를 통해 요청 데이터 처리
      - @CurrentUser를 활용하여 인증 사용자 정보를 간편히 주입
    - BoardService
      - 게시글 저장 후 첨부 파일 데이터를 Attachment 엔티티로 변환하여 저장
      - 트랜잭션 내에서 게시글과 첨부 파일 저장 처리

---
+++ src/main/java/com/sk/domain/board/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.sk.domain.board.repository;
+
+import com.sk.domain.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+
+}
