8850979692a0c0dcc1dceab6bc15b1fb75c6426d
yezy <iyeahji@gmail.com>
2024-12-01 13:44:14 +0900

    feat: AuthRepository에 findByEmail 메서드 추가
    - 로그인 시 사용자 조회에 활용

--- src/main/java/com/yezyaa/sk/domain/auth/repository/AuthRepository.java
+++ src/main/java/com/yezyaa/sk/domain/auth/repository/AuthRepository.java
@@ -3,8 +3,11 @@ package com.yezyaa.sk.domain.auth.repository;
 import com.yezyaa.sk.domain.auth.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 
 }
