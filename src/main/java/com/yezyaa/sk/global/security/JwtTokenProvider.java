881d8d3dc9f2afe13e936db92c697b6393400d6b
yezy <iyeahji@gmail.com>
2024-12-01 14:58:08 +0900

    enhance: JwtTokenProvider 기능 추가
    - validateToken 메서드: AccessToken 유효성 검증
    - extractUserId 메서드: 사용자 ID 추출
    - resolveToken 메서드: 요청 헤더에서 AccessToken 파싱

--- src/main/java/com/yezyaa/sk/global/security/JwtTokenProvider.java
+++ src/main/java/com/yezyaa/sk/global/security/JwtTokenProvider.java
@@ -1,10 +1,12 @@
 package com.yezyaa.sk.global.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -41,5 +43,35 @@ public class JwtTokenProvider {
                 .setExpiration(expiryDate) // 만료 시간
                 .signWith(key, SignatureAlgorithm.HS256) // 서명
                 .compact();
+    }
+
+    // AccessToken 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // 토큰에서 사용자 ID 추출
+    public Long extractUserId(String token) {
+        return Long.valueOf(Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+
+    // HTTP 요청 헤더에서 JWT 추출
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        return (bearerToken != null && bearerToken.startsWith("Bearer ")) ?
+                bearerToken.substring(7) : null;
     }
 }
