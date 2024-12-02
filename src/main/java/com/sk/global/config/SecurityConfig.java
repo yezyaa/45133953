f6a643c15330107a98a2e13b31e32ae7210c495a
yezy <iyeahji@gmail.com>
2024-12-02 23:13:35 +0900

    refactor: Spring Security 설정 리팩토링
    - AUTH_API_URL 상수 제거
      - 인증 관련 경로를 직접 requestMatchers에서 처리하도록 변경
    - 정적 리소스 경로 설정 변경
      - PathRequest.toStaticResources() 제거
      - "/css/**", "/js/**", "/images/**" 경로를 명시적으로 설정

--- src/main/java/com/sk/global/config/SecurityConfig.java
+++ src/main/java/com/sk/global/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.sk.global.config;
 
 import com.sk.global.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -22,11 +21,6 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private static final String[] AUTH_API_URL = {
-            "/api/auth",
-            "/api/auth/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -34,15 +28,20 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers(AUTH_API_URL).permitAll()
+                        .requestMatchers(
+                                "/h2-console/**",
+                                "/",
+                                "/index.html",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/board").permitAll()                  // 게시글 목록 조회
                         .requestMatchers(HttpMethod.GET, "/api/board/{boardId}").permitAll()        // 게시글 상세 조회
                         .requestMatchers(HttpMethod.POST, "/api/board").authenticated()             // 게시글 작성
                         .requestMatchers(HttpMethod.PUT, "/api/board/{boardId}").authenticated()    // 게시글 수정
                         .requestMatchers(HttpMethod.DELETE, "/api/board/{boardId}").authenticated() // 게시글 삭제
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 정적 리소스
-                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions().sameOrigin()) // H2 콘솔 iframe 허용
