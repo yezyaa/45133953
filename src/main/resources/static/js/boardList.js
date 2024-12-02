852f8c08429afd64b4a88062983daa3476d559fe
yezy <iyeahji@gmail.com>
2024-12-03 00:10:51 +0900

    feat: 게시판 페이지 기본 구조 구현
    - 게시판 테이블 구성: 번호, 제목, 첨부파일 여부, 작성자 ID, 작성일, 조회수
    - 페이지네이션 기본 구조 추가
    - 네비게이션 링크(홈, 로그인, 회원가입)를 JavaScript로 동적 생성
      - 로그인시 회원가입이 로그아웃으로 변경 되어야하기 때문

---
+++ src/main/resources/static/js/boardList.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.nav-links').innerHTML = `
+        <li><a href="/index.html">홈</a></li>
+        <li><a href="/signIn.html">로그인</a></li>
+        <li><a href="/signUp.html">회원가입</a></li>
+    `;
+});
